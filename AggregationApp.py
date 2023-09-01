5379ddbf1951b519e27039be2e7682efec4438c6
SiddharthMahendran <124019543+SiddharthMahendran@users.noreply.github.com>
2023-09-01 11:34:46 +0530

    Update AggregationApp.py

--- AggregationApp.py
+++ AggregationApp.py
@@ -15,6 +15,10 @@ uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 # Main app content
 st.title("Data Aggregation App")
 
+# Define df variable outside the if statement
+df = None
+selected_columns = None  # Define selected_columns initially
+
 if uploaded_file is not None:
     file_extension = uploaded_file.name.split(".")[-1]
 
@@ -24,24 +28,32 @@ if uploaded_file is not None:
         df = pd.read_excel(uploaded_file, engine="openpyxl")
     else:
         st.error("Unsupported file format")
+        st.stop()  # Stop execution to prevent further processing
 
-    # User interface for aggregation
-    if file_extension == "csv" or file_extension == "xlsx":
-        st.subheader("Select Columns and Aggregation Operation")
+    # Check for non-numeric data in selected columns
+    selected_columns = st.multiselect("Select columns for aggregation", df.columns)
 
-        selected_columns = st.multiselect("Select columns for aggregation", df.columns)
-        aggregation_option = st.radio("Select aggregation operation", ["SUM", "COUNT", "AVG"])
+    non_numeric_columns = []
+    for column in selected_columns:
+        if not pd.to_numeric(df[column], errors="coerce").notna().all():
+            non_numeric_columns.append(column)
 
-        if st.button("Perform Aggregation"):
-            st.subheader("Aggregated Results:")
-            if aggregation_option == "SUM":
-                st.write(df[selected_columns].sum())
-            elif aggregation_option == "COUNT":
-                st.write(df[selected_columns].count())
-            elif aggregation_option == "AVG":
-                st.write(df[selected_columns].mean())
+    if non_numeric_columns:
+        st.error(f"Unsupported data type in columns: {', '.join(non_numeric_columns)}")
+        st.stop()  # Stop execution if non-numeric characters are found
+        
+# User interface for aggregation
+st.subheader("Select Columns and Aggregation Operation")
 
+# Continue using the same selected_columns variable
+if selected_columns is not None:
+    aggregation_option = st.radio("Select aggregation operation", ["SUM", "COUNT", "AVG"])
 
-
-
-
+    if st.button("Perform Aggregation"):
+        st.subheader("Aggregated Results:")
+        if aggregation_option == "SUM":
+            st.write(df[selected_columns].sum())
+        elif aggregation_option == "COUNT":
+            st.write(df[selected_columns].count())
+        elif aggregation_option == "AVG":
+            st.write(df[selected_columns].mean())
